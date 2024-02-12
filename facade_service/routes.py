4901142e30f2d3bfbe26531472bd2aff39cedfc2
Bohdan213 <bohdan.pelekh@ucu.edu.ua>
2024-02-12 13:25:26 +0200

    Readme

--- facade_service/routes.py
+++ facade_service/routes.py
@@ -19,7 +19,7 @@ class FacadeService(Resource):
         response_logging = perform_post_request(url="http://127.0.0.1:8081/api/v1/logging_service",
                                         data={"msg": msg, "uuid": uuid})
         print(f"RESPONSE from the post request to logging service, status: {response_logging.status_code}")
-        return {"Status": "Success"}, 200
+        return {"Status": "Success", "uuid": uuid, "msg": msg}, 200
 
     def get(self):
         response_logging = perform_get_request(url="http://127.0.0.1:8081/api/v1/logging_service",
